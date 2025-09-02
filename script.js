24719d3f0b71003c2ec120cd5975520b38222113
neve1005 <00001090931001sp@al.educacao.sp.gov.br>
2025-09-02 10:32:47 -0300

    v1

--- script.js
+++ script.js
@@ -14,4 +14,13 @@ diminuirFonteBotao.addEventListener('clik', function(){
 
 
 
+
+const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+
+botaoDeAcessibilidade.addEventListener('click', function(){
+ botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+ opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+})
+
 })
